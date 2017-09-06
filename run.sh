b31a2b2fcb6180e0598f4727d2cd26d375636d9a
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-09-06 16:26:05 +0200

    ADDED: allow run with different argument (e.g. bash)

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 VERSION=3.1
+RUN=${1-./run.pl}
 sudo docker run \
 	-v $PWD:/opt/project \
 	-v /etc/group:/etc/group:ro \
 	-v /etc/passwd:/etc/passwd:ro \
 	-u $( id -u $USER ):$( id -g $USER ) \
 	-p 3020:3020 -it  \
-	jrvosse/cliopatria:$VERSION  ./run.pl
+	jrvosse/cliopatria:$VERSION $RUN
