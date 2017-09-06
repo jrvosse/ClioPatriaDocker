ef26561058f12e674bf7abe6a5da110703140bb4
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-09-06 10:12:07 +0200

    CHANGED: run configure as host user

--- config.sh
+++ config.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo docker run -v $PWD:/opt/project jrvosse/cliopatria:3.1 
+VERSION=3.1
+sudo docker run \
+	-v $PWD:/opt/project \
+	-v /etc/group:/etc/group:ro \
+	-v /etc/passwd:/etc/passwd:ro \
+	-u $( id -u $USER ):$( id -g $USER ) \
+	jrvosse/cliopatria:$VERSION 
