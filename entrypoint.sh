ccb4c6dcaa6a8c40449b31153041ab0bb5e3d98f
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-09-12 15:19:34 +0200

    CHANGED run ./daemon.pl as default entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Configure/run cliopatria in $PWD"
-[ ! -x ./run.pl ] && /opt/ClioPatria/configure --with-localhost
-[   -x ./run.pl ] && ./run.pl $@
+PORT=${PORT:-3020}
+[ ! -x ./daemon.pl ] && /opt/ClioPatria/configure --with-localhost
+[   -x ./daemon.pl ] && ./daemon.pl --no-fork --port=$PORT $@
