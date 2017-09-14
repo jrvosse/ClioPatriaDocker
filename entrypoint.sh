462abd9abb5c6304c425e6f4c60391c84b8de569
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-09-14 12:23:55 +0200

    more work in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-echo "$@"
 PORT=${PORT:-3020}
+echo "$@"
 
-# Make sure that ClioPatria is configured in the current directory:
-[ ! -x ./daemon.pl ] && /opt/ClioPatria/configure --with-localhost
+init() {
+	# Make sure that ClioPatria is configured in the current directory:
+	[ ! -x ./daemon.pl ] && /opt/ClioPatria/configure --with-localhost
+}
 
-# Make sure we are not running as root, run as nobody if we are root
-if [[ $1 == 'daemon' ]]; then
+cp_daemon() {
+	# Make sure we are not running as root, run as nobody if we are root
 	if [[ $EUID -ne 0 ]]; then
 		./daemon.pl --no-fork --port=$PORT $@
 	else
 		./daemon.pl --no-fork --port=$PORT --user=nobody $@
 	fi
-elif [[ $1 == 'cpack' ]]; then
-	./run.pl $@
-else 
-	echo "Fatal: $1 not implemented"
-fi
+}
+
+init
+case "$1" in
+	daemon)	cp_daemon
+		;;
+	rdf_load)
+		shift
+		./run.pl $@
+		cp_daemon
+		;;
+	cpack) 	./run.pl $@
+		cp_daemon
+		;;
+	*)	echo "Fatal: $1 not implemented";
+		exit 1;
+		;;
+esac
