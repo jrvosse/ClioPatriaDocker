d04b494e00119e2d5473b3712858c348a43819cd
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2019-03-28 16:11:49 +0100

    IMPROVED: used swish docker entry strategy to run the daemon in the containter under a dummy user with the same user id as the owner of the RDF-store directory on the host

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 CLIOPATRIA_DIR=${CLIOPATRIA_DIR:-/opt/ClioPatria}
+udaemon=daemon
 
 init() {
 	# Make sure that ClioPatria is configured in the current directory:
 	[ ! -x ./daemon.pl ] && /opt/ClioPatria/configure --with-localhost
 }
 
+# `mkuser file user` creates user with the uid and gid of file.
+
+mkuser()
+{ f="$1"
+  u="$2"
+
+  groupadd "$(ls -nd "$f" | awk '{printf "-g %s\n",$4 }')" -o $u
+  useradd  "$(ls -nd "$f" | awk '{printf "-u %s\n",$3 }')" -g $u -o $u
+}
+
 cp_daemon() {
-	# Make sure we are not running as root, run as nobody if we are root
+	# Make sure we are not running as root, run as $udaemon if we are root
 	if [[ $EUID -ne 0 ]]; then
 		CMD="./daemon.pl --no-fork $@ --port=3020"
 	else
-		CMD="./daemon.pl --no-fork --user=nobody $@ --port=3020"
+		CMD="./daemon.pl --no-fork --user=$udaemon $@ --port=3020"
 	fi
 	echo "Running cliopatria in daemon mode, use 'docker run -it jrvosse/cliopatria run' to run in interactive mode"
 	echo $CMD
 	$CMD
 }
+
+# If there is a RDF-store directory, reuse it and set our user to be the
+# native user of this directory.
+
+if [ -d RDF-store ]; then
+  mkuser RDF-store swish
+  udaemon=swish
+else
+  mkdir RDF-store
+  chown $udaemon.$udaemon RDF-store
+fi
 
 case "$1" in
 	daemon)	init
